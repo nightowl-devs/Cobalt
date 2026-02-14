70be6481a8c5b7c98705c48d08790c2fbcfc66a7
Nathan <209938737+quiteboring@users.noreply.github.com>
2026-02-14 16:44:42 -0500

    chore: remove unused import

--- src/main/kotlin/org/cobalt/api/command/CommandManager.kt
+++ src/main/kotlin/org/cobalt/api/command/CommandManager.kt
@@ -16,7 +16,6 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
-import net.minecraft.commands.CommandBuildContext
 import org.cobalt.api.command.annotation.DefaultHandler
 import org.cobalt.api.command.annotation.SubCommand
 
